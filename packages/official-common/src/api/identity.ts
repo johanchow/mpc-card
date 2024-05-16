5187af6606b8f368efbeb5dced8ac550016e4759
johan <zzrzhou@tencent.com>
2024-05-16 16:39:12 +0800

    feat: 调用一下接口试试

--- packages/official-common/src/api/identity.ts
+++ packages/official-common/src/api/identity.ts
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
-const getVerifyCode = async () => {
+const getVerifyCode = async (data: Record<string, string>) => {
+  console.log('start verify code: ', data);
+  const resp = await axios.post('//api.onlycoin.cc/SendEmail', {
+    data: {
+      email: data.email,
+    }
+  });
+  console.log('SendEmail resp: ', resp);
   await new Promise((resolve) => {
     setTimeout(resolve, 1000);
   });
